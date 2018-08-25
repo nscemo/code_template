cd404a8a4bb088be8d1d0091118be6871d0f8d9a
nscemo <rain_sys@126.com>
2018-08-25 12:17:18 +0800

    Update 循环左移.cpp

--- 循环左移.cpp
+++ 循环左移.cpp
@@ -1,3 +1,8 @@
+/*
+for (int i=1; i<10; ++i) myvector.push_back(i);   // 1 2 3 4 5 6 7 8 9
+
+std::reverse(myvector.begin(),myvector.end());    // 9 8 7 6 5 4 3 2 1
+*/
     void reverseString(string s, int from , int to)
     {
         while(from<to)
